86bcba228ee20f94adaffa4259c50b8b9e697ddc
plaumen <przemek.ciszak@gmail.com>
2019-01-01 20:55:12 +0100

    Add exception sample

--- CsharpSamples/Program.cs
+++ CsharpSamples/Program.cs
@@ -1,5 +1,5 @@
 ﻿using CsharpSamples.Common.Interfaces;
-using CsharpSamples.Samples.Basics.ValueAndReferenceTypes;
+using CsharpSamples.Samples.Basics.Exceptions;
 using System;
 
 namespace CsharpSamples
@@ -8,7 +8,7 @@ namespace CsharpSamples
     {
         static void Main(string[] args)
         {
-            ISample sample = new OutSample();
+            ISample sample = new ExceptionsSample();
             sample.Run();
 
             Console.ReadLine();
---
+++ CsharpSamples/Samples/Basics/Exceptions/ExceptionsSample.cs
@@ -0,0 +1,90 @@
+﻿using CsharpSamples.Common.Interfaces;
+using System;
+
+namespace CsharpSamples.Samples.Basics.Exceptions
+{
+    public class ExceptionsSample : ISample
+    {
+        public void Run()
+        {
+            TryCatchFinallyScenario();
+            ExceptionObjectUsedScenario();
+            CatchEverythingScenario();
+
+            // OUTPUT
+            // Base exception handled
+            // Finally block executed
+            // Derived exception Msg
+            // I am catching everything!
+        }
+
+        private void CatchEverythingScenario()
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch
+            {
+                Console.WriteLine("I am catching everything!");
+            }
+        }
+
+        private void ExceptionObjectUsedScenario()
+        {
+            try
+            {
+                throwDerivedException();
+            }
+            // you can specify variable name for exception and use it
+            catch (DerivedException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void TryCatchFinallyScenario()
+        {
+            try
+            {
+                throwBaseException();
+                Console.WriteLine("How you came here?");
+            }
+            catch (BaseException)
+            {
+                Console.WriteLine("Base exception handled");
+            }
+            finally
+            {
+                Console.WriteLine("Finally block executed");
+            }
+        }
+
+        private void throwBaseException()
+        {
+            throw new BaseException();
+        }
+
+        private void throwDerivedException()
+        {
+            throw new DerivedException("Derived exception Msg");
+        }
+    }
+
+    public class BaseException : Exception
+    {
+        public BaseException()
+        {
+        }
+    }
+
+    public class DerivedException : BaseException
+    {
+        public override string Message { get; }
+
+        public DerivedException(string message)
+        {
+            Message = message;
+        }
+    }
+}
