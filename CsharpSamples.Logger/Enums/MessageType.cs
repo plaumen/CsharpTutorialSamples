da74670851f97e108621f208390f778d1d437b35
pciszak <pciszak@sii.pl>
2019-01-14 15:45:17 +0100

    Add logger project and implementation

---
+++ CsharpSamples.Logger/Enums/MessageType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpSamples.Logger.Enums
+{
+    public enum MessageType
+    {
+        Debug,
+        Info,
+        Warning,
+        Error,
+        Fatal
+    }
+}
